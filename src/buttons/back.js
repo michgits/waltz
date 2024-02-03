bce17aa0d2fb8b0622d9a0fdd0a160b51904e621
mich <88956289+michgits@users.noreply.github.com>
2024-02-03 13:09:44 +0700

    Delete src directory

--- src/buttons/back.js
+++
@@ -1,9 +0,0 @@
-module.exports = async ({  inter, queue }) => { 
-    if (!queue || !queue.playing) return inter.reply({ content: `no music currently playing... try again ? ❌`, ephemeral: true });
-
-    if (!queue.previousTracks[1]) return inter.reply({ content: `there was no music played before ${inter.member}... try again ? ❌`, ephemeral: true });
-
-    await queue.back();
-
-    inter.reply({ content:`playing the **previous** track ✅`, ephemeral: true});
-}
--- src/buttons/skip.js
+++
@@ -1,7 +0,0 @@
-module.exports = async ({  inter, queue }) => { 
-    if (!queue || !queue.playing) return inter.reply({ content: `no music currently playing... try again ? ❌`, ephemeral: true });
-    
-    const success = queue.skip();
-
-    return inter.reply({ content: success ? `current music ${queue.current.title} skipped ✅` : `something went wrong ${inter.member}... try again ? ❌`, ephemeral: true});
-}
